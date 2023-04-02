450ea845229af52e6c50bf03d52b201e77d10728
Aryan Sawhney <80150109+sawhney17@users.noreply.github.com>
2023-04-02 23:23:48 +0400

    oopsies

--- main.py
+++ main.py
@@ -37,19 +37,6 @@ def hello_world():
     file_contents = blob.download_as_text(encoding="utf-8")
     # Make sure it's a json string
 
-    # Parse it as a JSON string
-    file_contents2 = json.loads(file_contents)
-    # Print the index_struct_id
-    print(file_contents2["vector_store"]["simple_vector_store_data_dict"]["text_id_to_doc_id"]["63bd4431-d6a7-4926-9267-b79a113947d0"])
-
-
-
-
-    # print the length of the string
-    print(len(file_contents))
-
-    # Return the first 100 characters of the string
-
     # return "Hello {}! Your file contents were: {}".format(name, file_contents)
     llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"))
 
@@ -57,8 +44,7 @@ def hello_world():
 
     response = index.query(messages, llm_predictor=llm_predictor)
 
-    print(response.get_formatted_sources())
-    return response.response()
+    return response.response
 
 
 if __name__ == "__main__":

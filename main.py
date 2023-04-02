eba73fdd26154661636bef7d20079da505767010
Aryan Sawhney <80150109+sawhney17@users.noreply.github.com>
2023-04-02 18:17:10 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from google.cloud import storage
+
+from flask import Flask, request
+
+app = Flask(__name__)
+bucket_name = os.environ.get("BUCKET_NAME", "newbucketismean")
+
+@app.route("/", methods=["POST"])
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    
+    # Create a Cloud Storage client object
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+
+    # Get a blob object representing the file you want to read
+    blob = bucket.blob("econTragakes.json")
+
+    # Read the contents of the file
+    file_contents = blob.download_as_string()
+
+    return "Hello {}! Your file contents were: {}".format(name, file_contents)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
